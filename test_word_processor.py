07b7a5b3aa2a1f1116eac18f862ed1d183064cd4
batulu1987315@163.com <hshy1987>
2015-02-04 17:29:44 +0800

    水木爬虫

--- test_word_processor.py
+++ test_word_processor.py
@@ -1,14 +1,17 @@
 from word_processor2 import wordprocess,command
 
-list = ['df','r','xyz','d','2','undo','test','redo','undo','undo']
-wp = wordprocess()
-for item in list:
-    if item == 'd':
-        wp.del_com()
-    elif item == 'undo':
-        wp.undo_com()
-    elif item == 'redo':
-        wp.redo_com()
-    else:
-        wp.add_com(item)
-print wp.word_list
+list = ['df','r','xyz','d','2','undo','test','redo','undo','undo'] #anwser ['df', 'r', 'xyz', 'test']
+list2 = ['first', 'second', 'third','d','fourth','undo','undo','redo']
+def test(list):
+    wp = wordprocess()
+    for item in list:
+        if item == 'd':
+            wp.del_com()
+        elif item == 'undo':
+            wp.undo_com()
+        elif item == 'redo':
+            wp.redo_com()
+        else:
+            wp.add_com(item)
+        print wp.word_list
+test(list2)
